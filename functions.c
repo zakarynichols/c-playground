8a80627607fae02224586b5e34996d57a52987d2
Zakary Nichols <zakarynichols@gmail.com>
2023-03-25 14:40:42 -0400

    declare prototype

--- functions.c
+++ functions.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int plus_one(int n) {
-    return n + 1;
-}
+int plus_one(int n);
 
 int main(void) {
     printf("%d\n", plus_one(0));
+}
+
+int plus_one(int n) {
+    return n + 1;
 }
