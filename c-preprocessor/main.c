1a7621258f37611979de4bbab4e50b76c166ffdd
Zakary Nichols <zakarynichols@gmail.com>
2023-04-01 23:01:13 -0400

    built in marcos

--- c-preprocessor/main.c
+++ c-preprocessor/main.c
@@ -4,4 +4,10 @@
 
 int main(void) {
     printf("LOCAL expands to: %s\n", LOCAL);
+
+    printf("This function: %s\n", __func__);
+    printf("This file: %s\n", __FILE__);
+    printf("This line: %d\n", __LINE__);
+    printf("Compiled on: %s %s\n", __DATE__, __TIME__);
+    printf("C Version: %ld\n", __STDC_VERSION__);
 }
