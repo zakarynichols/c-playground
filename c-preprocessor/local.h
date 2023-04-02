26e8a6d007170640b51f0ecc0e6471a3149ab909
Zakary Nichols <zakarynichols@gmail.com>
2023-04-01 22:53:18 -0400

    system-wide and local includes

---
+++ c-preprocessor/local.h
@@ -0,0 +1,6 @@
+#ifndef LOCAL_H
+#define LOCAL_H
+
+#define LOCAL "I'm a string from local.h"
+
+#endif
---
+++ c-preprocessor/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // Brackets indicate system-wide directory
+
+#include "local.h"  // Quotes indicate local files
+
+int main(void) {
+    printf("LOCAL expands to: %s\n", LOCAL);
+}
