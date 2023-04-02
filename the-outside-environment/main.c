b044f694085b09b7f432faf2b035ccf105f54fe5
Zakary Nichols <zakarynichols@gmail.com>
2023-04-01 21:16:55 -0400

    add cmd line args

--- the-outside-environment/main.c
+++ the-outside-environment/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     printf("%s\n", "The outside environment");
@@ -6,5 +7,15 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; i++) {
         printf("arg %d: %s\n", i, argv[i]);
     }
+
+    int total = 0;
+
+    for (int i = 1; i < argc; i++) {  // Start at 1, the first argument
+        int value = atoi(argv[i]);    // Use strtol() for better error handling
+
+        total += value;
+    }
+
+    printf("%d\n", total);
     return 0;
 }
