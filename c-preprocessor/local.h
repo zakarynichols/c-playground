179e4ab107bee238b3a3507c805f82ef3006f40d
Zakary Nichols <zakarynichols@gmail.com>
2023-04-01 23:29:57 -0400

    macros with one argument

--- c-preprocessor/local.h
+++ c-preprocessor/local.h
@@ -3,4 +3,8 @@
 
 #define LOCAL "I'm a string from local.h"
 
+// Parenthesis is so common that you should automatically
+// do it every time you make a parameterized math macro
+#define SQR(x) ((x) * (x))
+
 #endif
--- c-preprocessor/main.c
+++ c-preprocessor/main.c
@@ -3,8 +3,8 @@
 #include "local.h"  // Quotes indicate local files
 
 int main(void) {
+    printf("SQR = %d\n", SQR(12));
     printf("LOCAL expands to: %s\n", LOCAL);
-
     printf("This function: %s\n", __func__);
     printf("This file: %s\n", __FILE__);
     printf("This line: %d\n", __LINE__);
