89cdfb5277495119520664e6a769a83a97c6a0df
Zakary Nichols <zakarynichols@gmail.com>
2023-04-01 23:39:06 -0400

    macros with more than one argument

--- c-preprocessor/local.h
+++ c-preprocessor/local.h
@@ -1,10 +1,18 @@
 #ifndef LOCAL_H
 #define LOCAL_H
 
+#include <math.h>
+
+#define QUADP(a, b, c) ((-(b) + sqrt((b) * (b)-4 * (a) * (c))) / (2 * (a)))
+#define QUADM(a, b, c) ((-(b)-sqrt((b) * (b)-4 * (a) * (c))) / (2 * (a)))
+#define QUAD(a, b, c) QUADP(a, b, c), QUADM(a, b, c)
+
 #define LOCAL "I'm a string from local.h"
 
 // Parenthesis is so common that you should automatically
 // do it every time you make a parameterized math macro
 #define SQR(x) ((x) * (x))
+
+#define TRIANGLE_AREA(w, h) (0.5 * (w) * (h))
 
 #endif
--- c-preprocessor/main.c
+++ c-preprocessor/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>  // Brackets indicate system-wide directory
 
 #include "local.h"  // Quotes indicate local files
@@ -10,4 +11,7 @@ int main(void) {
     printf("This line: %d\n", __LINE__);
     printf("Compiled on: %s %s\n", __DATE__, __TIME__);
     printf("C Version: %ld\n", __STDC_VERSION__);
+
+    printf("2*x^2 + 10*x + 5 = 0\n");
+    printf("x = %f or x = %f\n", QUAD(2, 10, 5));
 }
