a7f1a02f29ec01352ef17586121fa13f37a2b28c
Zakary Nichols <zakarynichols@gmail.com>
2023-04-04 01:34:20 -0400

    adjacent bit fields

--- more-structs/bit_fields.c
+++ more-structs/bit_fields.c
@@ -16,4 +16,19 @@ struct bar {
 int main(void) {
     printf("foo: %zu\n", sizeof(struct foo));  // 16
     printf("bar: %zu\n", sizeof(struct bar));  // 4
+
+    // Non-adjacent bit fields
+
+    // A gotcha : C will only combine adjacent bit - fields.
+    // If they’re interrupted by non - bit - fields, you get no savings:
+    struct baz {             // sizeof(struct foo) == 16 (for me)
+        unsigned int a : 1;  // since a is not adjacent to c.
+        unsigned int b;
+        unsigned int c : 1;
+        unsigned int d;
+    };
+
+    printf("baz = %zu\n", sizeof(struct baz));  // 16
+
+    // Put all your bitfields together to get the compiler to combine them.
 }
