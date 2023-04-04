3babd3595d79025571751d802b83f43809981f45
Zakary Nichols <zakarynichols@gmail.com>
2023-04-03 23:54:26 -0400

    self-referential structs

--- more-structs/more_structs.c
+++ more-structs/more_structs.c
@@ -1,5 +1,12 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+// Linked list node
+struct node {
+    int data;
+    struct node *next;
+};
 
 // Anonymous struct used for later
 typedef struct {
@@ -93,6 +100,22 @@ int main(void) {
 
     d.leg_count = 2;
     d.name = "organism";
+
+    struct node *head;
+
+    // Hackishly set up a linked list (11)->(22)->(33)
+    head = malloc(sizeof(struct node));
+    head->data = 11;
+    head->next = malloc(sizeof(struct node));
+    head->next->data = 22;
+    head->next->next = malloc(sizeof(struct node));
+    head->next->next->data = 33;
+    head->next->next->next = NULL;
+
+    // Traverse it
+    for (struct node *cur = head; cur != NULL; cur = cur->next) {
+        printf("%d\n", cur->data);
+    }
 
     return 0;
 }
